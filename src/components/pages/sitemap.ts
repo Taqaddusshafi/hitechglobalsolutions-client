0d9431ab35828b59fa4403a2e17f8a4dba1b9640
Taqaddus <taqaddusshafi9086@gmail.com>
2025-11-03 15:46:15 +0530

    sitemap_chnages

---
+++ src/components/pages/sitemap.ts
@@ -0,0 +1,45 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      <url>
+        <loc>https://hitechglobals.com</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>weekly</changefreq>
+        <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>https://hitechglobals.com/about</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://hitechglobals.com/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://hitechglobals.com/services</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>https://hitechglobals.com/projects</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+    </urlset>
+  `
+}
+
+export default function SiteMap(req: NextApiRequest, res: NextApiResponse) {
+  const sitemap = generateSiteMap()
+  res.setHeader('Content-Type', 'text/xml; charset=utf-8')
+  res.write(sitemap)
+  res.end()
+}
